f1a7e428000534011a58f2b90d5d2dab69ff0b0f
atops <alan.toppen@gmail.com>
2018-11-20 13:56:31 -0500

    Additional config files

---
+++ get_included_detectors.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  7 22:20:12 2018
+
+@author: V0010894
+"""
+
+import pandas as pd
+import yaml
+import os
+import sqlalchemy as sq
+from datetime import datetime, timedelta
+import pyodbc
+import time
+from glob import glob
+
+
+with open('Monthly_Report_calcs.yaml') as yaml_file:
+        conf = yaml.load(yaml_file)
+
+start_date = (datetime.today() - timedelta(days=365)).strftime('%Y-%m-%d')
+end_date = (datetime.today() - timedelta(days=1)).strftime('%Y-%m-%d')
+dates = pd.date_range(start_date, end_date, freq='3D')
+        
+query = """SELECT DISTINCT SignalID, EventParam as Detector
+           FROM Controller_Event_Log WHERE EventCode = 82
+           AND Timestamp between '{} 08:00:00' and '{} 09:00:00';
+           """
+
+if os.name=='nt':
+        
+    uid = os.environ['VDOT_ATSPM_USERNAME']
+    pwd = os.environ['VDOT_ATSPM_PASSWORD']
+    
+    engine = sq.create_engine('mssql+pyodbc://{}:{}@sqlodbc'.format(uid, pwd),
+                              pool_size=20)
+
+elif os.name=='posix':
+
+    def connect():
+        return pyodbc.connect(
+            'Driver=FreeTDS;' + 
+            'SERVER={};'.format(os.environ['VDOT_ATSPM_SERVER_INSTANCE']) +
+            #'DATABASE={};'.format(os.environ['VDOT_ATSPM_DB']) +
+            'PORT=1433;' +
+            'UID={};'.format(os.environ['VDOT_ATSPM_USERNAME']) +
+            'PWD={};'.format(os.environ['VDOT_ATSPM_PASSWORD']) +
+            'TDS_Version=8.0;')
+    
+    engine = sq.create_engine('mssql://', creator=connect)
+    
+
+with engine.connect() as conn:
+
+    
+    for date_ in dates:
+        
+        t0 = time.time()
+        
+        sd = date_.strftime('%Y-%m-%d')
+        ed = sd #(date_ + pd.DateOffset(days=1)).strftime('%Y-%m-%d')
+        
+        print(sd, end=': ')
+            
+        df = pd.read_sql(sql=query.format(sd, ed), con=conn)
+        df.to_csv('included_detectors_{}.csv'.format(sd))
+        
+        print('{} sec'.format(time.time() - t0))
+    
+    filenames = glob('included_detectors_*.csv')
+    df = pd.concat([pd.read_csv(fn)[['SignalID','Detector']] for fn in filenames]).drop_duplicates()
+    df.SignalID = df.SignalID.astype('int')
+    
+    df.to_csv('included_detectors.csv')
+    
+    for fn in filenames:
+        os.remove(fn)
---
+++ nightly_config.py
@@ -0,0 +1,170 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  2 12:02:05 2018
+
+@author: V0010894
+"""
+
+import pandas as pd
+import numpy as np
+from datetime import datetime
+import boto3
+
+from get_maxtime_config_asyncio_polling import get_veh_det_plans, get_unit_info
+from get_maxtime_config_reduce import reduce_det_plans, reduce_unit_info
+from get_atspm_detectors import get_atspm_detectors
+
+
+# get Good/Authoritative Detector Config (det_config) and write to feather
+def get_det_config(adc, mdp, date_string):
+    # -- ---------------------------------
+    # -- Included detectors --
+    #  SignalID/Detector pairs with vol > 0 over the past year
+    #  over a sample of dates between 8am-9am
+    incld = (pd.read_csv('included_detectors.csv')
+                .sort_values(['SignalID','Detector'])
+                .set_index(['SignalID','Detector']))
+    
+    # -- -------------------------------------------------- --
+    # -- ATSPM Detector Config (from reduce function above) --
+    
+    adc = ad[['SignalID',
+             'Detector',
+             'ProtectedPhaseNumber',
+             'PermissivePhaseNumber',
+             'TimeFromStopBar',
+             'IPAddress']]
+    adc = adc.rename(columns={'ProtectedPhaseNumber': 'ProtPhase',
+                            'PermissivePhaseNumber': 'PermPhase'})
+    adc['CallPhase'] = np.where(adc.ProtPhase > 0, adc.ProtPhase, adc.PermPhase)
+    
+    try:
+        adc = adc[adc.SignalID != 'null']
+    except:
+        pass
+    adc = adc[~adc.Detector.isna()]
+    adc = adc[~adc.CallPhase.isna()]
+    
+    adc.SignalID = adc.SignalID.astype('int')
+    adc.Detector = adc.Detector.astype('int')
+    adc.CallPhase = adc.CallPhase.astype('int')
+    
+    adc = adc.set_index(['SignalID','Detector'])
+    
+    # Exclude those not in "included detectors"
+    #adc = adc.join(incld)
+    #adc = adc[~adc['Unnamed: 0'].isna()].drop(columns=['Unnamed: 0'])
+    
+    # -- --------------------------------------------------- --
+    # -- Maxtime Detector Plans (from reduce function above) --
+    
+    mdp = mvdp[['SignalID','Detector','CallPhase']]
+    
+    mdp.SignalID = mdp.SignalID.astype('int')
+    mdp.Detector = mdp.Detector.astype('int')
+    mdp.CallPhase = mdp.CallPhase.astype('int')
+    
+    mdp = mdp.set_index(['SignalID','Detector'])
+    
+    # Exclude those not in "included detectors"
+    mdp = mdp.join(incld).rename(columns={'Unnamed: 0': 'in_cel'})
+    mdp = mdp[~mdp['in_cel'].isna()]
+    
+    det_config = adc.join(other=mdp, how='outer', lsuffix='_atspm', rsuffix='_maxtime').sort_index()
+    
+    det_config.TimeFromStopBar = det_config.TimeFromStopBar.fillna(0).round(1)
+    det_config['CallPhase'] = np.where(det_config.CallPhase_maxtime.isna(),
+                                       det_config.CallPhase_atspm,
+                                       det_config.CallPhase_maxtime).astype('int')
+    
+    det_config[((~det_config.CallPhase_maxtime.isna()) & (~det_config.in_cel.isna()))]
+    
+    det_config = det_config[((~det_config.CallPhase_atspm.isna()) & (~det_config.CallPhase_maxtime.isna())) | ((~det_config.CallPhase_maxtime.isna()) & (~det_config.in_cel.isna()))]
+    # -- --------------------------------------------------------- --
+    # -- Combine ATSPM Detector Config and MaxTime Detector Config --
+    
+    det_config = det_config.reset_index()[['SignalID',
+                                       'Detector',
+                                       'CallPhase',
+                                       'TimeFromStopBar',
+                                       'CallPhase_atspm',
+                                       'CallPhase_maxtime',
+                                       'in_cel']]
+    return det_config
+
+s3 = boto3.client('s3')
+
+date_string = datetime.today().strftime('%Y-%m-%d')
+
+
+
+get_veh_det_plans()
+mvdp = reduce_det_plans()
+mvdp_csv_filename = 'MaxTime_Det_Plans_{}.csv'.format(date_string)
+mvdp.to_csv(mvdp_csv_filename)
+
+#upload to s3
+key = 'maxtime_det_plans/date={}/MaxTime_Det_Plans.csv'.format(date_string)
+s3.upload_file(Filename=mvdp_csv_filename, 
+               Bucket='gdot-devices', 
+               Key=key)
+
+
+
+
+ad = get_atspm_detectors()
+ad_csv_filename = 'ATSPM_Det_Config_{}.csv'.format(date_string)
+ad.to_csv(ad_csv_filename)
+
+# upload to s3
+key = 'atspm_det_config/date={}/ATSPM_Det_Config.csv'.format(date_string)
+s3.upload_file(Filename=ad_csv_filename, 
+               Bucket='gdot-devices', 
+               Key=key)
+
+
+
+get_unit_info()
+ui = reduce_unit_info()
+ui_csv_filename = 'MaxTime_Unit_Info_{}.csv'.format(date_string)
+ui.to_csv(ui_csv_filename)
+
+# upload to s3
+key = 'maxtime_unit_info/date={}/MaxTime_Unit_Info.csv'.format(date_string)
+s3.upload_file(Filename=ui_csv_filename, 
+               Bucket='gdot-devices', 
+               Key=key)
+
+
+
+
+det_config = get_det_config(ad, mvdp, date_string)
+dc_filename = 'ATSPM_Det_Config_Good_{}.feather'.format(date_string)
+det_config.to_feather(dc_filename)
+
+# upload to s3
+key = 'atspm_det_config_good/date={}/ATSPM_Det_Config_Good.feather'.format(date_string)
+s3.upload_file(Filename=dc_filename, 
+               Bucket='gdot-devices', 
+               Key=key)
+
+
+"""
+# Code to go back and calculate past days
+
+mvdp = reduce_det_plans()
+ad = get_atspm_detectors()
+det_config = get_det_config(ad, mvdp, date_string)
+
+
+dates = pd.date_range('2018-08-01', '2018-11-11', freq='1D')
+
+for date_string in [d.strftime('%Y-%m-%d') for d in dates]:
+    
+    ad = pd.read_csv('ATSPM_Det_Config_{}.csv'.format(date_string))
+    mvdp = pd.read_csv('MaxTime_Det_Plans_{}.csv'.format(date_string))
+    
+    det_config = get_det_config(ad, mvdp, date_string)
+    dc_filename = 'ATSPM_Det_Config_Good_{}.feather'.format(date_string)
+    det_config.to_feather(dc_filename)
+"""
