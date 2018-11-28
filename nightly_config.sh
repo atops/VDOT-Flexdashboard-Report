0f6de534d50964304f805b0ffcc767e89a9b5df4
atops <alan.toppen@gmail.com>
2018-11-28 12:40:32 -0500

    Updated gititnore. Added bash script

---
+++ nightly_config.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#/home/atoppen/Code/VDOT/restart_vpn.sh
+/home/atoppen/miniconda3/bin/python /home/atoppen/Code/VDOT/VDOT-Flexdashboard-Report/nightly_config.py
+/home/atoppen/Code/VDOT/flush_dns.sh
