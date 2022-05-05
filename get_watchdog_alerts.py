222f6e58e0a4baf89a7a30e8e63675ea312b809f
atops <alan.toppen@gmail.com>
2022-05-05 14:08:39 -0400

    Updated watchdog_alerts. s3io and parquet

--- get_watchdog_alerts.py
+++ get_watchdog_alerts.py
@@ -10,114 +10,91 @@ Created on Thu Jul 26 14:36:14 2018
 import pandas as pd
 import numpy as np
 import sqlalchemy as sq
-import yaml
+import pyodbc
 import os
+import io
 import boto3
 import zipfile
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 import polling
-import io
+from retrying import retry
+import yaml
 
-from pull_atspm_data import get_atspm_engine
+from s3io import *
+from databases import get_atspm_engine, get_maxv_engine, get_mvel_engine
+from mark1_logger import mark1_logger
 
+base_path = '.'
 
-# Not used. Kept because I don't want to have to rewrite it.
-def query_athena(query, database, output_bucket):
+logs_path = os.path.join(base_path, 'logs')
+if not os.path.exists(logs_path):
+    os.mkdir(logs_path)
+logger = mark1_logger(os.path.join(logs_path, f'get_watchdog_alerts_{datetime.today().strftime("%F")}.log'))
 
-    response = ath.start_query_execution(
-        QueryString=query,
-        QueryExecutionContext={
-            'Database': database
-        },
-        ResultConfiguration={
-            'OutputLocation': 's3://{}'.format(output_bucket)
-        }
-    )
-    print('Started query.')
-    # Wait for s3 object to be created
-    polling.poll(
-            lambda: 'Contents' in s3.list_objects(Bucket=output_bucket,
-                                                  Prefix=response['QueryExecutionId']),
-            step=0.2,
-            timeout=60)
-    print('Query complete.')
-    key = '{}.csv'.format(response['QueryExecutionId'])
-    time.sleep(1)
-    s3.download_file(Bucket=output_bucket, Key=key, Filename=key)
-    df = pd.read_csv(key)
-    os.remove(key)
-
-    print('Results downloaded.')
-    return df
+pd.options.display.max_columns = 10
+s3 = boto3.client('s3')
+s3r = boto3.resource('s3')
 
 
 # Read Corridors File from S3
-def get_corridors(s3, bucket, key):
-
-    with io.BytesIO() as data:
-        s3.download_fileobj(
-                Bucket=bucket,
-                Key=key, Fileobj=data)
-
-        corridors = pd.read_feather(data)
-        corridors = (corridors[~corridors.SignalID.isna()]
-            .assign(SignalID=lambda x: x.SignalID.astype('int'))
-            .drop(['Description'], axis=1))
+def get_corridors(bucket, filename):
+    filename = os.path.splitext(filename)[0] + '.feather'
+    corridors = read_feather(Bucket=bucket, Key=filename)
+    corridors = (corridors[~corridors.SignalID.isna()]
+                .assign(SignalID = lambda x: x.SignalID.astype('int'))
+                .drop(['Description'], axis=1))
 
     return corridors
 
 
 # Upload watchdog alerts to predetermined location in S3
-def s3_upload_watchdog_alerts(s3, df, bucket, region):
+@retry(wait_random_min=1000, wait_random_max=2000, stop_max_attempt_number=10)
+def s3_upload_watchdog_alerts(df, conf, asof_date=None):
+  
+    s3key = 'mark/interim/watchdog/SPMWatchDogErrorEvents.parquet'
+    if asof_date:
+        s3key = s3key.replace('.parquet', f'_{asof_date}.parquet')
 
-    feather_filename = f'SPMWatchDogErrorEvents_{region}.feather'
-    zipfile_filename = feather_filename + '.zip'
-
-    df.reset_index(drop=True).to_feather(feather_filename)
-
-    # Compress file
-    zf = zipfile.ZipFile(zipfile_filename, 'w', zipfile.ZIP_DEFLATED)
-    zf.write(feather_filename)
-    zf.close()
-
-    # Upload compressed file to s3
-    s3.upload_file(Filename=zipfile_filename,
-                   Bucket=bucket,
-                   Key='mark/watchdog/{}'.format(zipfile_filename))
-    os.remove(feather_filename)
-    os.remove(zipfile_filename)
+    write_parquet(df, Bucket=conf['bucket'], Key=s3key)
 
 
-def get_watchdog_alerts(engine, corridors):
+@retry(wait_random_min=1000, wait_random_max=2000, stop_max_attempt_number=10)
+def get_watchdog_alerts(engine, corridors, asof_date=None):
 
     # Query ATSPM Watchdog Alerts Table from ATSPM
     with engine.connect() as conn:
-        SPMWatchDogErrorEvents = pd.read_sql_table('SPMWatchDogErrorEvents', con=conn)\
+        query = 'SELECT * FROM SPMWatchDogErrorEvents' 
+        if asof_date:
+            query = query + f" WHERE TimeStamp >= '{asof_date}'"
+ 
+        SPMWatchDogErrorEvents = pd.read_sql_query(query, con=conn)\
             .drop(columns=['ID'])\
             .drop_duplicates()
-
+    
     # Join Watchdog Alerts with Corridors
     wd = SPMWatchDogErrorEvents.loc[SPMWatchDogErrorEvents.SignalID != 'null', ]
+    wd = wd.loc[wd.TimeStamp > datetime.today() - timedelta(days=100)]
     wd = wd.fillna(value={'DetectorID': '0'})
     wd['Detector'] = np.vectorize(
             lambda a, b: a.replace(b, ''))(wd.DetectorID, wd.SignalID)
     wd = wd.drop(columns=['DetectorID'])
+    wd = wd.loc[wd.SignalID.isin(corridors.SignalID.astype('str'))]
     wd.SignalID = wd.SignalID.astype('int')
-
+    
     wd = (wd.set_index(['SignalID']).join(corridors.set_index(['SignalID']), how = 'left')
             .reset_index())
     wd = wd[~wd.Corridor.isna()]
     wd = wd.rename(columns = {'Phase': 'CallPhase',
                               'TimeStamp': 'Date'})
-
+    
     # Clearn up the Message into a new field: Alert
     wd.loc[wd.Message.str.contains('Force Offs'), 'Alert'] = 'Force Offs'
     wd.loc[wd.Message.str.contains('Count'), 'Alert'] = 'Count'
     wd.loc[wd.Message.str.contains('Max Outs'), 'Alert'] = 'Max Outs'
     wd.loc[wd.Message.str.contains('Pedestrian Activations'), 'Alert'] = 'Pedestrian Activations'
     wd.loc[wd.Message.str.contains('Missing Records'), 'Alert'] = 'Missing Records'
-
+    
     # Enforce Data Types
     wd.Alert = wd.Alert.astype('str')
     wd.Detector = wd.Detector.astype('int')
@@ -126,63 +103,50 @@ def get_watchdog_alerts(engine, corridors):
     wd.ErrorCode = wd.ErrorCode.astype('category')
     wd.Zone = wd.Zone.astype('category')
     wd.Zone_Group = wd.Zone_Group.astype('category')
-
+    
     wd.Corridor = wd.Corridor.astype('category')
     wd.Name = wd.Name.astype('category')
     wd.Date = wd.Date.dt.date
-
-    wd = wd.filter(['Zone_Group', 'Zone', 'Corridor',
-                    'SignalID', 'CallPhase', 'Detector',
+    
+    wd = wd.reset_index(drop=True)
+    wd = wd.filter(['Zone_Group', 'Zone', 'Corridor', 
+                    'SignalID', 'CallPhase', 'Detector', 
                     'Alert', 'Name', 'Date'], axis = 1)
 
     return wd
-    #Zone_Group | Zone | Corridor | SignalID/CameraID | CallPhase | Detector | Date | Alert | Name
 
 
 def main():
-
+   
+    with open('Monthly_Report_interim.yaml') as yaml_file:
+        conf = yaml.load(yaml_file, Loader=yaml.Loader)
+ 
     now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-    with open('Monthly_Report_AWS.yaml') as yaml_file:
-        cred = yaml.load(yaml_file, Loader=yaml.Loader)
+    try:    
+        atspm_engine = get_atspm_engine()
+        corridors = get_corridors(conf['bucket'], conf['corridors_filename_s3'])
     
-    with open('Monthly_Report.yaml') as yaml_file:
-        conf = yaml.load(yaml_file, Loader=yaml.Loader)
+        asof_date = (pd.Timestamp.today() - pd.Timedelta(7, unit='days')) - pd.offsets.MonthBegin(4)
+        asof_date = asof_date.strftime('%F')
 
-    s3 = boto3.client('s3', verify=conf['ssl_cert'])
-    ath = boto3.client('athena', verify=conf['ssl_cert'])
-    s3r = boto3.resource('s3', verify=conf['ssl_cert'])
+        wd = get_watchdog_alerts(atspm_engine, corridors, asof_date)
+        logger.success('Watchdog alerts successfully queried')
+    
+    except Exception as e:
+        logger.error(f'Could not query watchdog alerts - {str(e)}')
 
     try:
-        engine = get_atspm_engine(
-            username=cred['ATSPM_UID'], 
-            password=cred['ATSPM_PWD'], 
-            hostname=cred['ATSPM_HOST'], 
-            database=cred['ATSPM_DB'],
-            dsn=cred['ATSPM_DSN'])
-
-        corridors = get_corridors(
-                s3, 
-                conf['bucket'],
-                conf['corridors_filename_s3'].replace('.xlsx','.feather'))
-
-        wd = get_watchdog_alerts(engine, corridors)
-        print('{now} - {alerts} watchdog alerts'.format(now=now, alerts=len(wd)))
-
-        try:
-            # Write to Feather file - WatchDog
-            s3_upload_watchdog_alerts(s3, wd, conf['bucket'], conf['region'])
-            print('{now} - successfully uploaded to s3'.format(now=now))
-        except Exception as e:
-            print('{now} - ERROR: Could not upload to s3 - {err}'.format(now=now, err=e))
+        # Write to s3 - WatchDog
+        s3_upload_watchdog_alerts(wd, conf) # don't append date to file. only maintain one file.
+        logger.success('Watchdog alerts successfully uploaded to s3')
 
     except Exception as e:
-        print('{now} - ERROR: Could not retrieve watchdog alerts - {err}'.format(now=now, err=e))
-
+        logger.error(f'Could not save watchdog alerts to s3 - {str(e)}')
+    
 
 if __name__=='__main__':
     main()
-
 
 
 
