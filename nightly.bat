2136afa7ff935e2e2fb8a11aaf0d17f7e2b84e74
atops <alan.toppen@gmail.com>
2023-01-22 00:36:25 +0000

    Update nightly.bat to run all package scripts.

--- nightly.bat
+++ nightly.bat
@@ -1,3 +1,4 @@
-C:\Users\V0010894\R\R-3.5.1\bin\Rscript.exe Monthly_Report_calcs.R
-DocumentClient.exe TEAMS_Reports\tasks.csv
-C:\Users\V0010894\R\R-3.5.1\bin\Rscript.exe Monthly_Report_Package.R 
+"C:\Program Files\R\R-4.2.2\bin\Rscript.exe" Monthly_Report_Calcs.R
+"C:\Program Files\R\R-4.2.2\bin\Rscript.exe" Monthly_Report_Package.R
+"C:\Program Files\R\R-4.2.2\bin\Rscript.exe" Monthly_Report_Package_1hr.R
+"C:\Program Files\R\R-4.2.2\bin\Rscript.exe" Monthly_Report_Package_15min.R
