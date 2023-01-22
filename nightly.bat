23ee2e27a070006ec4f8d31247604f40eed78a75
atops <alan.toppen@gmail.com>
2023-01-22 18:54:52 +0000

    Updates to nightly batch script to run all Calcs and Package scripts

--- nightly.bat
+++ nightly.bat
@@ -1,4 +1,14 @@
-"C:\Program Files\R\R-4.2.2\bin\Rscript.exe" Monthly_Report_Calcs.R
-"C:\Program Files\R\R-4.2.2\bin\Rscript.exe" Monthly_Report_Package.R
-"C:\Program Files\R\R-4.2.2\bin\Rscript.exe" Monthly_Report_Package_1hr.R
-"C:\Program Files\R\R-4.2.2\bin\Rscript.exe" Monthly_Report_Package_15min.R
+echo %date%%time%
+for /f "tokens=1-4 delims=/ " %%i in ("%date%") do (
+     set dow=%%i
+     set month=%%j
+     set day=%%k
+     set year=%%l
+)
+set datestr=%year%%month%%day%
+echo datestr is %datestr% > nightly_last_run.txt
+
+"C:\Program Files\R\R-4.2.2\bin\Rscript.exe" Monthly_Report_Calcs.R >> logs\nightly_%datestr%.log
+"C:\Program Files\R\R-4.2.2\bin\Rscript.exe" Monthly_Report_Package.R >> logs\nightly_%datestr%.log
+"C:\Program Files\R\R-4.2.2\bin\Rscript.exe" Monthly_Report_Package_1hr.R >> logs\nightly_%datestr%.log
+"C:\Program Files\R\R-4.2.2\bin\Rscript.exe" Monthly_Report_Package_15min.R >> logs\nightly_%datestr%.log
