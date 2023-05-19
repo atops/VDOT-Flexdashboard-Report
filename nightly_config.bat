63edef06e8251fd48f92892f5279657390caeca6
atops <alan.toppen@gmail.com>
2023-05-19 12:50:44 +0000

    Updated nightly batch files. R-4.2.3. Differs from staging. Need to fix.

--- nightly_config.bat
+++ nightly_config.bat
@@ -6,8 +6,10 @@ for /f "tokens=1-4 delims=/ " %%i in ("%date%") do (
      set year=%%l
 )
 set datestr=%year%%month%%day%
+del nightly_config_last_run.txt
 echo datestr is %datestr% > nightly_config_last_run.txt
 
 call activate.bat
 cd c:\TractionMetrics\production\
+del logs\nightly_config_%datestr%.log
 conda run -n tractionmetrics python nightly_config.py > logs\nightly_config_%datestr%.log
