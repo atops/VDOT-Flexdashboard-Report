2a4d8bcd144b4604cb6bfd8ec80fef9dfb2df87b
atops <alan.toppen@gmail.com>
2023-09-06 12:53:12 +0000

    Revert nightly_config.bat. Just needed to add miniconda to the PATH environment variable.

--- nightly_config.bat
+++ nightly_config.bat
@@ -6,14 +6,8 @@ for /f "tokens=1-4 delims=/ " %%i in ("%date%") do (
      set year=%%l
 )
 set datestr=%year%%month%%day%
-del nightly_config_last_run.txt
 echo datestr is %datestr% > nightly_config_last_run.txt
 
-call c:\ProgramData\miniconda3\Scripts\activate.bat
+call activate.bat
 cd c:\TractionMetrics\production\
-call c:\ProgramData\miniconda3\Scripts\conda activate tractionmetrics
-call C:\Users\alan_toppen\.conda\envs\tractionmetrics\python nightly_config.py && (
-  echo nightly.py was successful >> nightly_config_last_run.txt
-) || (
-  echo nightly.py failed >> nightly_config_last_run.txt
-)
+conda run -n tractionmetrics python nightly_config.py > logs/nightly_config_%datestr%_%random%.log
